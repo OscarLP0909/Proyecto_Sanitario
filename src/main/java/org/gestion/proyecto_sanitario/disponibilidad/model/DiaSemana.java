89ae891bf2364a458cc5b437e8b56b27f5ac4f22
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-05-26 12:57:00 +0200

    Disponibilidad Model

---
+++ src/main/java/org/gestion/proyecto_sanitario/disponibilidad/model/DiaSemana.java
@@ -0,0 +1,6 @@
+package org.gestion.proyecto_sanitario.disponibilidad.model;
+
+public enum DiaSemana {
+    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+
+}
---
+++ src/main/java/org/gestion/proyecto_sanitario/disponibilidad/model/Disponibilidad.java
@@ -0,0 +1,49 @@
+package org.gestion.proyecto_sanitario.disponibilidad.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.gestion.proyecto_sanitario.medico.model.Medico;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "disponibilidades")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Disponibilidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_medico", nullable = false)
+    private Medico medico;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DiaSemana diaSemana;
+
+    @Column(nullable = false)
+    private LocalDateTime horaInicio;
+
+    @Column(nullable = false)
+    private LocalDateTime horaFin;
+
+    @Column(nullable = false)
+    private Integer duracionMinutos;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
